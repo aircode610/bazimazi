ad1a9ec664034683f1a97437d86e756b72dbfdd5
aircode610 <34022796+aircode610@users.noreply.github.com>
2020-06-28 17:28:01 +0430

    Update settings.py

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -128,4 +128,4 @@ EMAIL_HOST_USER = "aircode610@gmail.com"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = "@Aircode610"
+EMAIL_HOST_PASSWORD = "@Amirali1385"
